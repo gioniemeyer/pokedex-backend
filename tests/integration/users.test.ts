fbc2bb084472441346bc37bc500ccca6ecc0306f
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-08-31 11:41:29 -0300

    tests of sign-up

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -1,4 +1,4 @@
-
+import "../../src/setup";
 import supertest from "supertest";
 import generateSignUpBody from "../factories/userFactory";
 import app from "../../src/app";
@@ -8,12 +8,27 @@ beforeAll(startConnection);
 afterAll(endConnection);
 beforeEach(clearDatabase);
 
-describe("GET /sign-up", () => {
+describe("POST /sign-up", () => {
 	it("should answer with text \"OK!\" and status 201", async () => {
 
 		const user = generateSignUpBody();
-		console.log(user);
 		const response = await supertest(app).post("/sign-up").send(user);
 		expect(response.status).toBe(201);
+	});
+
+	it("should answer with text \"OK!\" and status 400", async () => {
+
+		const user = generateSignUpBody();
+		user.email = "email-errado";
+		const response = await supertest(app).post("/sign-up").send(user);
+		expect(response.status).toBe(400);
+	});
+
+	it("should answer with text \"OK!\" and status 409", async () => {
+
+		const user = generateSignUpBody();
+		await supertest(app).post("/sign-up").send(user);
+		const response = await supertest(app).post("/sign-up").send(user);
+		expect(response.status).toBe(409);
 	});
 });
